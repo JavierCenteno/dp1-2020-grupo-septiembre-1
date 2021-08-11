14dae10b1e6841530390a5947cf74b59de11eccc
Javier Centeno Vega <jacenve@telefonica.net>
2021-08-11 19:56:31 +0200

    Add relations to data

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,4 +1,12 @@
 --------------------------------------------------------------------------------
+-- Buildings
+
+INSERT INTO buildings(id,address,income,name) VALUES (1,'c/Building 1',0,'building1');
+INSERT INTO buildings(id,address,income,name) VALUES (2,'c/Building 2',0,'building2');
+INSERT INTO buildings(id,address,income,name) VALUES (3,'c/Building 3',0,'building3');
+INSERT INTO buildings(id,address,income,name) VALUES (4,'c/Building 4',0,'building4');
+
+--------------------------------------------------------------------------------
 -- Managers
 
 INSERT INTO users(username,password,enabled) VALUES ('manager1','manager1',TRUE);
@@ -22,27 +30,27 @@ INSERT INTO managers(id,name,email,address,username) VALUES (4, 'Diane Manager',
 
 INSERT INTO users(username,password,enabled) VALUES ('employee1','employee1',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (5,'employee1','employee');
-INSERT INTO employees(id,name,email,address,building_id,username) VALUES (1, 'Alice Employee', 'alice@employee.com', 'c/Employee 1', null, 'employee1');
+INSERT INTO employees(id,name,email,address,building_id,username) VALUES (1, 'Alice Employee', 'alice@employee.com', 'c/Employee 1', 1, 'employee1');
 
 INSERT INTO users(username,password,enabled) VALUES ('employee2','employee2',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (6,'employee2','employee');
-INSERT INTO employees(id,name,email,address,building_id,username) VALUES (2, 'Bob Employee', 'bob@employee.com', 'c/Employee 2', null, 'employee2');
+INSERT INTO employees(id,name,email,address,building_id,username) VALUES (2, 'Bob Employee', 'bob@employee.com', 'c/Employee 2', 1, 'employee2');
 
 INSERT INTO users(username,password,enabled) VALUES ('employee3','employee3',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (7,'employee3','employee');
-INSERT INTO employees(id,name,email,address,building_id,username) VALUES (3, 'Charles Employee', 'charles@employee.com', 'c/Employee 3', null, 'employee3');
+INSERT INTO employees(id,name,email,address,building_id,username) VALUES (3, 'Charles Employee', 'charles@employee.com', 'c/Employee 3', 1, 'employee3');
 
 INSERT INTO users(username,password,enabled) VALUES ('employee4','employee4',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (8,'employee4','employee');
-INSERT INTO employees(id,name,email,address,building_id,username) VALUES (4, 'Diane Employee', 'diane@employee.com', 'c/Employee 4', null, 'employee4');
+INSERT INTO employees(id,name,email,address,building_id,username) VALUES (4, 'Diane Employee', 'diane@employee.com', 'c/Employee 4', 1, 'employee4');
 
 INSERT INTO users(username,password,enabled) VALUES ('employee5','employee5',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (9,'employee5','employee');
-INSERT INTO employees(id,name,email,address,building_id,username) VALUES (5, 'Ernest Employee', 'ernest@employee.com', 'c/Employee 5', null, 'employee5');
+INSERT INTO employees(id,name,email,address,building_id,username) VALUES (5, 'Ernest Employee', 'ernest@employee.com', 'c/Employee 5', 2, 'employee5');
 
 INSERT INTO users(username,password,enabled) VALUES ('employee6','employee6',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (10,'employee6','employee');
-INSERT INTO employees(id,name,email,address,building_id,username) VALUES (6, 'Fatima Employee', 'fatima@employee.com', 'c/Employee 6', null, 'employee6');
+INSERT INTO employees(id,name,email,address,building_id,username) VALUES (6, 'Fatima Employee', 'fatima@employee.com', 'c/Employee 6', 2, 'employee6');
 
 INSERT INTO users(username,password,enabled) VALUES ('employee7','employee7',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (11,'employee7','employee');
@@ -53,25 +61,17 @@ INSERT INTO authorities(id,username,authority) VALUES (12,'employee8','employee'
 INSERT INTO employees(id,name,email,address,building_id,username) VALUES (8, 'Hector Employee', 'hector@employee.com', 'c/Employee 8', null, 'employee8');
 
 --------------------------------------------------------------------------------
--- Buildings
-
-INSERT INTO buildings(id,address,income,name) VALUES (1,'c/Building 1',0,'building1');
-INSERT INTO buildings(id,address,income,name) VALUES (2,'c/Building 2',0,'building2');
-INSERT INTO buildings(id,address,income,name) VALUES (3,'c/Building 3',0,'building3');
-INSERT INTO buildings(id,address,income,name) VALUES (4,'c/Building 4',0,'building4');
-
---------------------------------------------------------------------------------
--- Tools
-
-INSERT INTO tools(id,name,building_id,task_id) VALUES (1,'tool1',1,null);
-INSERT INTO tools(id,name,building_id,task_id) VALUES (2,'tool2',1,null);
-INSERT INTO tools(id,name,building_id,task_id) VALUES (3,'tool3',2,null);
-INSERT INTO tools(id,name,building_id,task_id) VALUES (4,'tool4',2,null);
-
---------------------------------------------------------------------------------
 -- Tasks
 
 INSERT INTO tasks(id,complete,income,name) VALUES (1,false,1000,'task1');
 INSERT INTO tasks(id,complete,income,name) VALUES (2,false,2000,'task2');
 INSERT INTO tasks(id,complete,income,name) VALUES (3,false,3000,'task3');
 INSERT INTO tasks(id,complete,income,name) VALUES (4,false,4000,'task4');
+
+--------------------------------------------------------------------------------
+-- Tools
+
+INSERT INTO tools(id,name,building_id,task_id) VALUES (1,'tool1',1,1);
+INSERT INTO tools(id,name,building_id,task_id) VALUES (2,'tool2',1,2);
+INSERT INTO tools(id,name,building_id,task_id) VALUES (3,'tool3',2,null);
+INSERT INTO tools(id,name,building_id,task_id) VALUES (4,'tool4',2,null);
