8151dc7a057191c85206fc84a3786b20df298eaf
Javier Centeno Vega <jacenve@telefonica.net>
2021-08-17 19:24:35 +0200

    Add tool test and fix a bug

--- src/test/java/org/springframework/samples/petclinic/service/ToolServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ToolServiceTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.petclinic.model.Employee;
+import org.springframework.samples.petclinic.model.Task;
 import org.springframework.samples.petclinic.model.Tool;
 import org.springframework.samples.petclinic.util.EntityUtils;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,12 @@ class ToolServiceTests {
 
 	@Autowired
 	protected ToolService toolService;
+
+	@Autowired
+	protected TaskService taskService;
+
+	@Autowired
+	protected EmployeeService employeeService;
 
 	////////////////////////////////////////////////////////////////////////////////
 	// Tests
@@ -38,6 +46,30 @@ class ToolServiceTests {
 		assertThat(tool3.getName()).isEqualTo("tool3");
 		Tool tool4 = EntityUtils.getById(tools, Tool.class, 4);
 		assertThat(tool4.getName()).isEqualTo("tool4");
+	}
+
+	@Test
+	void shouldFindAllToolsAssignableToTask() {
+		Tool tool1 = this.toolService.findToolById(1).get();
+		Task task1 = this.taskService.findTaskById(1).get();
+		Employee employee1 = this.employeeService.findEmployeeById(1).get();
+		Collection<Tool> tools;
+		// task1 no tiene edificio asignado aún
+		// así que debe tener 0 herramientas asignables
+		tools = this.toolService.findAssignableToTask(1);
+		assertThat(tools.size()).isEqualTo(0);
+		// task1 tiene ahora el edificio de employee1 (building1)
+		// así que debe tener asignables todas las herramientas de building1
+		task1.addEmployee(employee1);
+		this.taskService.saveTask(task1);
+		tools = this.toolService.findAssignableToTask(1);
+		assertThat(tools.size()).isEqualTo(2);
+		// a task1 se le ha asignado tool1
+		// así que tool1 ya no es asignable a task1, el total de herramientas es 1 menos
+		tool1.setTask(task1);
+		this.toolService.saveTool(tool1);
+		tools = this.toolService.findAssignableToTask(1);
+		assertThat(tools.size()).isEqualTo(1);
 	}
 
 	@Test
